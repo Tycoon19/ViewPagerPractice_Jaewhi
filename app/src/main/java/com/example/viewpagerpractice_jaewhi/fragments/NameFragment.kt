a82b0ebb76e98bea4bfe0172915d850880013326
Tycoon19 <kevin960831@naver.com>
2021-12-20 20:46:52 +0900

    뷰페이저에 배치될 3개의 프래그먼트 완성

---
+++ app/src/main/java/com/example/viewpagerpractice_jaewhi/fragments/NameFragment.kt
@@ -0,0 +1,27 @@
+package com.example.viewpagerpractice_jaewhi.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpagerpractice_jaewhi.R
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+}
