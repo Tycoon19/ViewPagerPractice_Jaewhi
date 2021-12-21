5fa6853612da45e8243062b9219b6b4cab44927f
Tycoon19 <kevin960831@naver.com>
2021-12-22 00:00:52 +0900

    탭레이아웃 배치 -> 뷰페이저와 연동

--- app/src/main/java/com/example/viewpagerpractice_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice_jaewhi/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.viewpagerpractice_jaewhi.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 //  나중에 찐으로 담아줄께 약속만 해두고 onCreate{}안에서 실제로 객체화해서 담아주자.
@@ -18,5 +19,7 @@ class MainActivity : AppCompatActivity() {
         mainViewPager.adapter = mViewPagerAdapter
 //      페이지 수가 많아지면 간단하게 부드럽도록 지원하는 기능.
         mainViewPager.offscreenPageLimit = 5
+
+        myTabLayout.setupWithViewPager(mainViewPager)
     }
 }
